980bdff6c468f531a261406f4d454d1087f66dfe
loressl <loryssl@hotmail.com>
2019-06-12 00:46:13 -0300

    Adicionado trecho de código para o cliente 2.

--- src/Socket/chat/Cliente2.java
+++ src/Socket/chat/Cliente2.java
@@ -1,7 +1,30 @@
 package Socket.chat;
 
-public class Cliente2 {
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class Cliente2 {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		Socket cliente = new Socket("192.168.15.3", 1234);
+		System.out.println("Cliente 2 conectado!");
+
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+
+		saida.println("Servidor, aqui que é cliente 2 e meu ip é " + cliente.getInetAddress());
+
+		Scanner lerServidor = new Scanner(cliente.getInputStream());
+
+		System.out.println(lerServidor.nextLine());
+
+		entrada.close();
+		saida.close();
+		cliente.close();
 	}
 }
