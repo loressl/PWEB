817364262ae9ca2eaec76c7f16ba3b40bce630de
loressl <loryssl@hotmail.com>
2019-07-01 13:42:34 -0300

    Socket básico simulando a conversa entre cliente e servidor.

---
+++ src/cliente_servidor/Cliente.java
@@ -0,0 +1,61 @@
+package cliente_servidor;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Cliente {
+	
+	public void go() {
+		try {
+			System.out.println("Iniciando cliente.");
+			System.out.println("Iniciando conexão com o servidor.");
+			
+			Socket socket = new Socket("127.0.0.1", 1234);
+			System.out.println("Conexão estabelecida.");
+
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+			
+			BufferedReader ler = new BufferedReader(new InputStreamReader(input));
+			PrintStream escrita = new PrintStream(output);
+			
+			Scanner teclado = new Scanner(System.in);
+			
+			while(true) {
+				
+				System.out.print("Digite uma mensagem cliente: ");
+				String mensagem = teclado.nextLine();
+				
+				escrita.println(mensagem);
+				
+				if("FIM".equals(mensagem))
+					break;
+				
+				mensagem = ler.readLine();
+				
+				System.out.println("Mensagem recebida do servidor: "+ mensagem);
+			}
+			
+			System.out.println("Encerrando conexão.");
+			
+			ler.close();
+			escrita.close();
+			socket.close();
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Cliente().go();
+	}
+}
---
+++ src/cliente_servidor/Servidor.java
@@ -0,0 +1,69 @@
+package cliente_servidor;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+
+public class Servidor {
+	
+	public void go() {
+		try {
+
+			System.out.println("Iniciando conexão");
+			ServerSocket serverSocket = new ServerSocket(1234);
+			System.out.println("Aguardando conexão.");
+			
+			Socket socket = serverSocket.accept();
+			System.out.println("Conexão estabelecida");
+			
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(input));
+			PrintStream escrita = new PrintStream(socket.getOutputStream());
+			
+			Scanner teclado = new Scanner(System.in);
+			
+			while (true) {
+				String mensagem = in.readLine();
+				
+				System.out.println("Mensagem recebida do cliente["+
+								socket.getInetAddress().getHostName() + "]: "
+								+ mensagem);
+				
+				
+				if("FIM".equals(mensagem)) {
+					break;
+				}
+				
+				System.out.print("Digite uma mensagem servidor: ");
+				String enviar = teclado.nextLine();
+				escrita.println(enviar);
+			}
+			
+			System.out.println("Encerrando conexão.");
+			
+			in.close();
+			escrita.close();
+			socket.close();
+			
+			System.out.println("Encerrando servidor.");
+			serverSocket.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Servidor().go();
+	}
+}
